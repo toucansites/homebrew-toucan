bbc11a5b46c33ea59b4884cd9e6f0fb95d2d4e6c
GErP83 <gurrka@gmail.com>
2025-05-21 19:12:19 +0200

    Update toucan.rb

--- Formula/toucan.rb
+++ Formula/toucan.rb
@@ -4,10 +4,10 @@ class Toucan < Formula
   version "1.0.0-beta.4"
 
   if OS.mac?
-    url "https://github.com/toucansites/toucan/archive/refs/tags/toucan-macos-1.0.0.beta.4.zip"
+    url "https://github.com/toucansites/toucan/releases/download/1.0.0-beta.4/toucan-macos-1.0.0.beta.4.zip"
     sha256 "a1bf2cb273aafd11c8ecc14fa3fb910216403935066b2ab43c1b050273bd1b89"
   elsif OS.linux?
-    url "https://github.com/toucansites/toucan/archive/refs/tags/toucan-linux-1.0.0.beta.4.zip"
+    url "https://github.com/toucansites/toucan/releases/download/1.0.0-beta.4/toucan-linux-1.0.0.beta.4.zip"
     sha256 "b985329227f10e5c11f85531aef4d4404d097bd0f1346bbffdee956b3c13df82"
   end
 
