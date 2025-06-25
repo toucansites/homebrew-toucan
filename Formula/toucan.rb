ef2594576e3a416c4830b845d2c794bd5b602e90
GErP83 <gurrka@gmail.com>
2025-06-25 14:59:03 +0200

    update to toucan beta5

--- Formula/toucan.rb
+++ Formula/toucan.rb
@@ -1,14 +1,14 @@
 class Toucan < Formula
   desc "Toucan is a static site generator written in Swift"
   homepage "https://github.com/toucansites/toucan"
-  version "1.0.0-beta.4"
+  version "1.0.0-beta.5"
 
   if OS.mac?
-    url "https://github.com/toucansites/toucan/releases/download/1.0.0-beta.4/toucan-macos-1.0.0.beta.4.zip"
-    sha256 "a1bf2cb273aafd11c8ecc14fa3fb910216403935066b2ab43c1b050273bd1b89"
+    url "https://github.com/toucansites/toucan/releases/download/1.0.0-beta.5/toucan-macos-1.0.0.beta.5.zip"
+    sha256 "c35d16f30e0acd254213e82c7bd49c52e4df3b5983f4c225b198bd72da02640a"
   elsif OS.linux?
-    url "https://github.com/toucansites/toucan/releases/download/1.0.0-beta.4/toucan-linux-1.0.0.beta.4.zip"
-    sha256 "b985329227f10e5c11f85531aef4d4404d097bd0f1346bbffdee956b3c13df82"
+    url "https://github.com/toucansites/toucan/releases/download/1.0.0-beta.5/toucan-linux-1.0.0.beta.5.zip"
+    sha256 "3b71aefd779205a1df888c9433785d854a62fa3df6ed7ac43204a71b9447bcea"
   end
 
   def install
